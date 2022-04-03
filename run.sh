9069cdf17d8f5d351c5242d2bffa23418d142c32
Yulin Zhang <ylzhang0008@gmail.com>
2022-04-03 15:04:12 -0500

    update

---
+++ run.sh
@@ -0,0 +1,2 @@
+#CUDA_VISIBLE_DEVICES=-1 python train_curriculum.py --config configs/maml/mini_grid_curriculum.yaml --output-folder maml-mini-grid --seed 1 --num-workers 8 --wandb --name="mini-grid"
+python train_curriculum.py --config configs/maml/mini_grid_curriculum.yaml --output-folder maml-mini-grid --seed 1 --num-workers 8 --wandb --name="mini-grid"
