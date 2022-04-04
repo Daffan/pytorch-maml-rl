d6514697d7cd2f52dfc88015aa53112ac7eb7d7d
Yulin Zhang <ylzhang0008@gmail.com>
2022-04-04 17:57:52 -0500

    fix zero division issue

--- train_curriculum.py
+++ train_curriculum.py
@@ -83,12 +83,21 @@ def main(args):
                                 ls_backtrack_ratio=config['ls-backtrack-ratio'])
 
         train_episodes, valid_episodes = sampler.sample_wait(futures)
-        num_iterations += sum(sum(episode.lengths) for episode in train_episodes[0])
+        if train_episodes is not None and len(train_episodes) != 0:
+            num_iterations += sum(sum(episode.lengths) for episode in train_episodes[0])
         num_iterations += sum(sum(episode.lengths) for episode in valid_episodes)
-        logs.update(tasks=tasks,
-                    num_iterations=num_iterations,
-                    train_returns=get_returns(train_episodes[0]),
-                    valid_returns=get_returns(valid_episodes))
+
+        if train_episodes is not None and len(train_episodes) != 0:
+            logs.update(tasks=tasks,
+                        num_iterations=num_iterations,
+                        train_returns=get_returns(train_episodes[0]),
+                        valid_returns=get_returns(valid_episodes))
+        else:
+            logs.update(tasks=tasks,
+                        num_iterations=num_iterations,
+                        train_returns=0,
+                        valid_returns=get_returns(valid_episodes))
+
 
         # Save policy
         if args.output_folder is not None:
